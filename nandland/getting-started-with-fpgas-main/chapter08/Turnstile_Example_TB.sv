3f68a5186bcf18cdf6a2763f22d270e14b4e7408
Jinto Jolly James <jintojollyjames@outlook.com>
2025-11-29 16:58:06 -0700

    Add files via upload

---
+++ nandland/getting-started-with-fpgas-main/chapter08/Turnstile_Example_TB.sv
@@ -0,0 +1,41 @@
+module Turnstile_Example_TB();
+
+  reg r_Reset = 1'b1, r_Clk = 1'b0, r_Coin = 1'b0, r_Push = 1'b0;
+  wire w_Locked;
+
+  Turnstile_Example UUT
+   (.i_Reset(r_Reset),
+    .i_Clk(r_Clk), 
+    .i_Coin(r_Coin),
+    .i_Push(r_Push),
+    .o_Locked(w_Locked));
+  
+  always #1 r_Clk <= !r_Clk;
+
+  initial begin
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
+    #10;
+    r_Reset <= 1'b0;
+    #10;
+    assert (w_Locked == 1'b1);
+
+    r_Coin <= 1'b1;
+    #10;
+    assert (w_Locked == 1'b0);
+
+    r_Push <= 1'b1;
+    #10;
+    assert (w_Locked == 1'b1);
+
+    r_Coin <= 1'b0;
+    #10;
+    assert (w_Locked == 1'b1);
+
+    r_Push <= 1'b0;
+    #10;
+    assert (w_Locked == 1'b1);
+    $finish();
+  end
+
+endmodule
